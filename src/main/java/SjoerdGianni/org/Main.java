eadebaaa2425341042d8eb152671323dc8c5428e
Sjoerd Peeneman <92178883+0DarkPhoenix@users.noreply.github.com>
2026-04-09 20:09:38 +0200

    chore: Added launch for Yaeger in main()

--- src/main/java/SjoerdGianni/org/Main.java
+++ src/main/java/SjoerdGianni/org/Main.java
@@ -5,8 +5,8 @@ import com.github.hanyaeger.api.YaegerGame;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends YaegerGame {
-    static void main() {
-
+    static void main(String[] args) {
+        launch(args);
     }
 
 
