cc1c3599e2d6d569b10b252f2760ca62d8e330ed
Gianni <gianni@eal.nl>
2026-05-04 20:42:13 +0200

    Updated UI

--- src/main/java/SjoerdGianni/org/scenes/EndScene.java
+++ src/main/java/SjoerdGianni/org/scenes/EndScene.java
@@ -28,33 +28,156 @@ public class EndScene extends StaticScene implements KeyListener {
 
     @Override
     public void setupEntities() {
-        // Scene label box
-        var sceneBox = new LabelBox(new Coordinate2D(10, 10), 200, 40);
-        addEntity(sceneBox);
-        
-        var sceneLabel = new TextEntity(new Coordinate2D(110, 30), "END SCENE");
-        sceneLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
-        sceneLabel.setFill(Color.BLACK);
-        sceneLabel.setFont(Font.font("Arial", FontWeight.BOLD, 16));
-        addEntity(sceneLabel);
-
+        // "GAME OVER" title with glow effect (using multiple overlaid texts)
         var gameOverText = new TextEntity(
-            new Coordinate2D(640, 300),
+            new Coordinate2D(640, 150),
             "GAME OVER"
         );
         gameOverText.setAnchorPoint(AnchorPoint.CENTER_CENTER);
         gameOverText.setFill(Color.RED);
-        gameOverText.setFont(Font.font("Arial", FontWeight.BOLD, 60));
+        gameOverText.setFont(Font.font("Arial", FontWeight.BOLD, 48));
         addEntity(gameOverText);
 
-        var instructionText = new TextEntity(
-            new Coordinate2D(640, 400),
-            "Press SPACE to return to title"
+        // Final Score label
+        var finalScoreLabel = new TextEntity(
+            new Coordinate2D(640, 230),
+            "FINAL SCORE"
         );
-        instructionText.setAnchorPoint(AnchorPoint.CENTER_CENTER);
-        instructionText.setFill(Color.LIGHTGRAY);
-        instructionText.setFont(Font.font("Arial", FontWeight.NORMAL, 30));
-        addEntity(instructionText);
+        finalScoreLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        finalScoreLabel.setFill(Color.LIGHTGRAY);
+        finalScoreLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(finalScoreLabel);
+
+        // Score value - display directly on black background (no box)
+        var scoreValue = new TextEntity(new Coordinate2D(640, 270), "1,234");
+        scoreValue.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        scoreValue.setFill(Color.WHITE);
+        scoreValue.setFont(Font.font("Arial", FontWeight.BOLD, 32));
+        addEntity(scoreValue);
+
+        // Difficulty label
+        var difficultyLabel = new TextEntity(
+            new Coordinate2D(640, 320),
+            "DIFFICULTY"
+        );
+        difficultyLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        difficultyLabel.setFill(Color.LIGHTGRAY);
+        difficultyLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(difficultyLabel);
+
+        // Difficulty value - display directly on black background
+        var difficultyValue = new TextEntity(new Coordinate2D(640, 352), "MEDIUM");
+        difficultyValue.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        difficultyValue.setFill(Color.WHITE);
+        difficultyValue.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        addEntity(difficultyValue);
+
+        // RESTART button - white box
+        var restartBox = new LabelBox(new Coordinate2D(475, 400), 130, 60);
+        addEntity(restartBox);
+        
+        var restartLabel = new TextEntity(new Coordinate2D(540, 422), "RESTART");
+        restartLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        restartLabel.setFill(Color.BLACK);
+        restartLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        addEntity(restartLabel);
+
+        var restartHint = new TextEntity(new Coordinate2D(540, 445), "Press R");
+        restartHint.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        restartHint.setFill(Color.DARKGRAY);
+        restartHint.setFont(Font.font("Arial", FontWeight.NORMAL, 12));
+        addEntity(restartHint);
+
+        // MENU button - white box
+        var menuBox = new LabelBox(new Coordinate2D(675, 400), 130, 60);
+        addEntity(menuBox);
+        
+        var menuLabel = new TextEntity(new Coordinate2D(740, 422), "MENU");
+        menuLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        menuLabel.setFill(Color.BLACK);
+        menuLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        addEntity(menuLabel);
+
+        var menuHint = new TextEntity(new Coordinate2D(740, 445), "Press M");
+        menuHint.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        menuHint.setFill(Color.DARKGRAY);
+        menuHint.setFont(Font.font("Arial", FontWeight.NORMAL, 12));
+        addEntity(menuHint);
+
+        // Game Statistics section
+        var statsTitle = new TextEntity(new Coordinate2D(640, 500), "GAME STATISTICS");
+        statsTitle.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        statsTitle.setFill(Color.WHITE);
+        statsTitle.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        addEntity(statsTitle);
+
+        // Outer container box for all statistics
+        var statsContainerBox = new LabelBox(new Coordinate2D(335, 520), 610, 170);
+        addEntity(statsContainerBox);
+
+        // Top Left: Enemies Killed
+        var enemiesKilledBox = new LabelBox(new Coordinate2D(350, 530), 200, 70);
+        addEntity(enemiesKilledBox);
+        
+        var enemiesKilledLabel = new TextEntity(new Coordinate2D(365, 550), "Enemies Killed");
+        enemiesKilledLabel.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        enemiesKilledLabel.setFill(Color.BLACK);
+        enemiesKilledLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(enemiesKilledLabel);
+
+        var enemiesKilledValue = new TextEntity(new Coordinate2D(365, 580), "[123]");
+        enemiesKilledValue.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        enemiesKilledValue.setFill(Color.BLACK);
+        enemiesKilledValue.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        addEntity(enemiesKilledValue);
+
+        // Top Right: Time Survived
+        var timeSurvivedBox = new LabelBox(new Coordinate2D(730, 530), 200, 70);
+        addEntity(timeSurvivedBox);
+        
+        var timeSurvivedLabel = new TextEntity(new Coordinate2D(745, 550), "Time Survived");
+        timeSurvivedLabel.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        timeSurvivedLabel.setFill(Color.BLACK);
+        timeSurvivedLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(timeSurvivedLabel);
+
+        var timeSurvivedValue = new TextEntity(new Coordinate2D(745, 580), "[2:34]");
+        timeSurvivedValue.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        timeSurvivedValue.setFill(Color.BLACK);
+        timeSurvivedValue.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        addEntity(timeSurvivedValue);
+
+        // Bottom Left: Power-ups Used
+        var powerupsUsedBox = new LabelBox(new Coordinate2D(350, 610), 200, 70);
+        addEntity(powerupsUsedBox);
+        
+        var powerupsUsedLabel = new TextEntity(new Coordinate2D(365, 630), "Power-ups Used");
+        powerupsUsedLabel.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        powerupsUsedLabel.setFill(Color.BLACK);
+        powerupsUsedLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(powerupsUsedLabel);
+
+        var powerupsUsedValue = new TextEntity(new Coordinate2D(365, 660), "[5]");
+        powerupsUsedValue.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        powerupsUsedValue.setFill(Color.BLACK);
+        powerupsUsedValue.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        addEntity(powerupsUsedValue);
+
+        // Bottom Right: Accuracy
+        var accuracyBox = new LabelBox(new Coordinate2D(730, 610), 200, 70);
+        addEntity(accuracyBox);
+        
+        var accuracyLabel = new TextEntity(new Coordinate2D(745, 630), "Accuracy");
+        accuracyLabel.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        accuracyLabel.setFill(Color.BLACK);
+        accuracyLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(accuracyLabel);
+
+        var accuracyValue = new TextEntity(new Coordinate2D(745, 660), "[45%]");
+        accuracyValue.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        accuracyValue.setFill(Color.BLACK);
+        accuracyValue.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        addEntity(accuracyValue);
     }
 
     @Override
--- src/main/java/SjoerdGianni/org/scenes/TitleScene.java
+++ src/main/java/SjoerdGianni/org/scenes/TitleScene.java
@@ -28,33 +28,102 @@ public class TitleScene extends StaticScene implements KeyListener {
 
     @Override
     public void setupEntities() {
-        // Scene label box
-        var sceneBox = new LabelBox(new Coordinate2D(10, 10), 200, 40);
-        addEntity(sceneBox);
-        
-        var sceneLabel = new TextEntity(new Coordinate2D(110, 30), "TITLE SCENE");
-        sceneLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
-        sceneLabel.setFill(Color.BLACK);
-        sceneLabel.setFont(Font.font("Arial", FontWeight.BOLD, 16));
-        addEntity(sceneLabel);
-
+        // Title at top
         var titleText = new TextEntity(
-            new Coordinate2D(640, 300),
+            new Coordinate2D(640, 150),
             "GEOMETRY WARS"
         );
         titleText.setAnchorPoint(AnchorPoint.CENTER_CENTER);
         titleText.setFill(Color.WHITE);
-        titleText.setFont(Font.font("Arial", FontWeight.BOLD, 60));
+        titleText.setFont(Font.font("Arial", FontWeight.BOLD, 48));
         addEntity(titleText);
 
-        var instructionText = new TextEntity(
-            new Coordinate2D(640, 400),
-            "Press SPACE to start"
+        // "Select Difficulty" label
+        var selectDifficultyLabel = new TextEntity(
+            new Coordinate2D(640, 240),
+            "Select Difficulty"
         );
-        instructionText.setAnchorPoint(AnchorPoint.CENTER_CENTER);
-        instructionText.setFill(Color.LIGHTGRAY);
-        instructionText.setFont(Font.font("Arial", FontWeight.NORMAL, 30));
-        addEntity(instructionText);
+        selectDifficultyLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        selectDifficultyLabel.setFill(Color.WHITE);
+        selectDifficultyLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
+        addEntity(selectDifficultyLabel);
+
+        var clickDifficultyHint = new TextEntity(
+            new Coordinate2D(640, 265),
+            "Click a difficulty to start"
+        );
+        clickDifficultyHint.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        clickDifficultyHint.setFill(Color.LIGHTGRAY);
+        clickDifficultyHint.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(clickDifficultyHint);
+
+        // EASY difficulty box
+        var easyBox = new LabelBox(new Coordinate2D(365, 310), 550, 55);
+        addEntity(easyBox);
+        
+        var easyLabel = new TextEntity(new Coordinate2D(390, 325), "EASY");
+        easyLabel.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        easyLabel.setFill(Color.BLACK);
+        easyLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        addEntity(easyLabel);
+
+        var easySpawnText = new TextEntity(new Coordinate2D(390, 350), "Enemies spawn every 3.5-7s");
+        easySpawnText.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        easySpawnText.setFill(Color.GRAY);
+        easySpawnText.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(easySpawnText);
+
+        // MEDIUM difficulty box
+        var mediumBox = new LabelBox(new Coordinate2D(365, 375), 550, 55);
+        addEntity(mediumBox);
+        
+        var mediumLabel = new TextEntity(new Coordinate2D(390, 390), "MEDIUM");
+        mediumLabel.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        mediumLabel.setFill(Color.BLACK);
+        mediumLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        addEntity(mediumLabel);
+
+        var mediumSpawnText = new TextEntity(new Coordinate2D(390, 415), "Enemies spawn every 2-5s");
+        mediumSpawnText.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        mediumSpawnText.setFill(Color.GRAY);
+        mediumSpawnText.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(mediumSpawnText);
+
+        // HARD difficulty box
+        var hardBox = new LabelBox(new Coordinate2D(365, 440), 550, 55);
+        addEntity(hardBox);
+        
+        var hardLabel = new TextEntity(new Coordinate2D(390, 455), "HARD");
+        hardLabel.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        hardLabel.setFill(Color.BLACK);
+        hardLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        addEntity(hardLabel);
+
+        var hardSpawnText = new TextEntity(new Coordinate2D(390, 480), "Enemies spawn every 1.5-3.5s");
+        hardSpawnText.setAnchorPoint(AnchorPoint.CENTER_LEFT);
+        hardSpawnText.setFill(Color.GRAY);
+        hardSpawnText.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        addEntity(hardSpawnText);
+
+        // Settings button (bottom left area)
+        var settingsBox = new LabelBox(new Coordinate2D(365, 540), 120, 40);
+        addEntity(settingsBox);
+        
+        var settingsLabel = new TextEntity(new Coordinate2D(425, 560), "Settings");
+        settingsLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        settingsLabel.setFill(Color.BLACK);
+        settingsLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 16));
+        addEntity(settingsLabel);
+
+        // Back to desktop button (bottom right area)
+        var backBox = new LabelBox(new Coordinate2D(795, 540), 120, 40);
+        addEntity(backBox);
+        
+        var backLabel = new TextEntity(new Coordinate2D(855, 560), "Back to desktop");
+        backLabel.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        backLabel.setFill(Color.BLACK);
+        backLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 13));
+        addEntity(backLabel);
     }
 
     @Override
